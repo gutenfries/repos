70e35e3919ebea84917ac5029436da4185dbe71d
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2022-02-12 22:58:09 -0600

    add elint files

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	root: true,
+	extends: ['eslint:recommended', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 2020,
+		parser: 'babel-eslint',
+	},
+	env: {
+		browser: true,
+		node: true,
+	},
+	plugins: ['prettier'],
+	globals: {},
+	rules: {
+		'prettier/prettier': 'warn',
+		'class-methods-use-this': 'off',
+		'no-param-reassign': 'off',
+		camelcase: 'off',
+		'no-unused-vars': 'off',
+	},
+};
